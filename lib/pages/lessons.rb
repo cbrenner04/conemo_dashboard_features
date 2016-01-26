9ab8173358d93c5f37af156838b55d0d89c57847
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-01-25 21:36:48 -0600

    Continue adding lessons specs

--- lib/pages/lessons.rb
+++ lib/pages/lessons.rb
@@ -6,8 +6,17 @@ class Lessons
     click_on 'Lessons'
   end
 
-  def add_a_lesson(title, day)
+  def assert_on_page
+    find('.btn', text: 'Add Lesson')
+  end
+
+  def open_add_lesson
     click_on 'Add Lesson'
+    find('#lesson_title')
+  end
+
+  def add_a_lesson(title, day)
+    open_add_lesson
     fill_in 'lesson[name]', with: title
     fill_in 'lesson[day_in_treatment]', with: day
     click_on 'Create Lesson'
@@ -17,9 +26,14 @@ class Lessons
     find('.table').has_css?('td', text: lesson)
   end
 
-  def add_a_slide(lesson, title, body)
+  def open_add_slide(lesson)
     open_lesson(lesson)
     click_on 'Add Slide'
+    find('#slide_title')
+  end
+
+  def add_a_slide(lesson, title, body)
+    open_add_slide(lesson)
     fill_in 'slide[title]', with: title
     fill_in '.cke_contents', body
     click_on 'Create Slide'
@@ -41,17 +55,84 @@ class Lessons
     find('p', text: body)
   end
 
-  def edit_lesson(lesson, new_title)
+  def open_edit_lesson(lesson)
     find('tr', text: lesson).find('.fa-pencil').click
+  end
+
+  def edit_lesson(lesson, new_title)
+    open_edit_lesson(lesson)
     fill_in 'lesson[title]', with: new_title
     click_on 'Update Lesson'
   end
 
-  def edit_slide(lesson, slide, new_title)
+  def open_edit_slide(lesson, slide)
     open_lesson(lesson)
     find('tr', text: slide).find('.fa-pencil').click
+  end
+
+  def edit_slide(lesson, slide, new_title)
+    open_edit_slide(lesson, slide)
     fill_in 'slide[title]', with: new_title
     click_on 'Update Slide'
+  end
+
+  def delete_lesson(lesson)
+    find('tr', text: lesson).find('.fa-trash-o').click
+    accept_alert "Are you sure you want to delete #{lesson}?"
+    find('.alert-info', text: 'Lesson deleted')
+  end
+
+  def delete_slide(lesson, slide)
+    open_lesson(lesson)
+    find('tr', text: slide).find('.fa-trash-o').click
+    accept_alert "Are you sure you want to delete #{slide}?"
+    find('.alert-info', text: 'Slide deleted')
+  end
+
+  def add_dialogue(title, day, message)
+    navigate_to_dialogues
+    click_on 'Add Dialogue'
+    fill_in 'dialogue[title]', text: title
+    fill_in 'dialogue[day_in_treatment]', text: day
+    fill_in 'dialogue[message]', text: message
+    fill_in 'dialogue[yes_text]', text: yes
+    fill_in 'dialogue[no_text]', text: no
+    click_on 'Create Dialogue'
+  end
+
+  def has_dialogue_visible?(dialogue)
+    navigate_to_dialogues
+    find('.table').has_css?('td', text: dialogue)
+  end
+
+  def read_dialogue(dialogue, message)
+    navigate_to_dialogues
+    click_on dialogue
+    find('h2', text: dialogue)
+    find('p', text: message)
+  end
+
+  def edit_dialogue(dialogue, new_title)
+    navigate_to_dialogues
+    find('tr', text: dialogue).find('.fa-pencil').click
+    fill_in 'dialogue[title]', text: new_title
+    click_on 'Update Dialogue'
+  end
+
+  def delete_dialogue(dialogue)
+    navigate_to_dialogues
+    find('tr', text: dialogue).find('.fa-trash-o').click
+    accept_alert "Are you sure you want to delete #{dialogue}?"
+  end
+
+  def navigate_with_breadcrumbs(lesson, slide)
+    open_lesson(lesson)
+    click_on slide
+    find('h2', text: slide)
+    within('.breadcrumbs') { click_on lesson }
+    find('h2', text: lesson)
+    within('.breadcrumbs') { click_on 'Lessons' }
+    assert_on_page
   end
 
   private
@@ -59,5 +140,9 @@ class Lessons
   def open_lesson(lesson)
     click_on lesson
     find('h2', text: lesson)
+  end
+
+  def navigate_to_dialogues
+    click_on 'Dialogues'
   end
 end
--- spec/features/lessons_spec.rb
+++ spec/features/lessons_spec.rb
@@ -17,24 +17,96 @@ describe 'An authorized admin logs in,' \
     login.sign_in(ENV['EN_Admin_Email'], ENV['EN_Admin_Password'])
     navigation.switch_to_english
     lessons.open
+    lessons.assert_on_page
   end
 
-  it 'cancels adding a lesson'
-  it 'adds a lesson'
-  it 'cancels adding a slide'
-  it 'adds a slide'
-  it 'reads a lesson'
-  it 'reads a slide'
-  it 'cancels editing a lesson'
-  it 'edits a lesson'
-  it 'cancels editing a slide'
-  it 'edits a slide'
-  it 'deletes a lesson'
-  it 'deletes a slide'
-  it 'adds a dialogue'
-  it 'reads a dialogue'
-  it 'edits a dialogue'
-  it 'deletes a dialogue'
-  it 'navigates using breadcrumbs from lesson'
-  it 'navigates using breadcrumbs from slide'
+  it 'cancels adding a lesson' do
+    lessons.open_add_lesson
+    navigation.cancel
+    lessons.assert_on_page
+  end
+
+  it 'adds a lesson' do
+    lessons.add_a_lesson('New Lesson', '15')
+    expect(lessons).to have_lesson_visible('New Lesson')
+  end
+
+  it 'cancels adding a slide' do
+    lessons.open_add_slide('Lesson 3')
+    navigation.cancel
+    expect(lessons).to have_slide_visible('Slide 1')
+  end
+
+  it 'adds a slide' do
+    lessons.add_a_slide('Lesson 11', 'New Slide', 'This is the slide body')
+    expect(lessons).to have_slide_visible('New Slide')
+  end
+
+  it 'reads a lesson' do
+    lessons.read_lesson('Lesson 5', 'Slide 1')
+  end
+
+  it 'reads a slide' do
+    lessons.read_slide('Lessons 6', 'Slide 1', 'read this')
+  end
+
+  it 'cancels editing a lesson' do
+    lessons.open_edit_lesson('Lesson 7')
+    navigation.cancel
+    lessons.assert_on_page
+  end
+
+  it 'edits a lesson' do
+    lessons.edit_lesson('Lesson 12', 'Edited Lesson')
+    expect(lessons).to have_lesson_visible('Edited Lesson')
+  end
+
+  it 'cancels editing a slide' do
+    lessons.open_edit_slide('Lesson 2', 'Slide 1')
+    navigation.cancel
+    expect(lessons).to have_slide_visible('Slide 2')
+  end
+
+  it 'edits a slide' do
+    lessons.edit_slide('Lesson 13', 'Slide 2', 'Edited Slide')
+    expect(lessons).to have_slide_visible('Edited Slide')
+  end
+
+  it 'deletes a lesson' do
+    lessons.delete_lesson('Lesson 14')
+    expect(lessons).to_not have_lesson_visible('Lesson 14')
+  end
+
+  it 'deletes a slide' do
+    lessons.delete_slide('Lesson 15', 'Slide 1')
+    expect(lessons).to_not have_slide_visible('Lesson 15')
+  end
+
+  it 'cancels adding a dialogue' do
+
+  end
+
+  it 'adds a dialogue' do
+
+  end
+
+  it 'reads a dialogue' do
+
+  end
+
+  it 'cancels editing a dialogue' do
+
+  end
+
+  it 'edits a dialogue' do
+
+  end
+
+  it 'deletes a dialogue' do
+
+  end
+
+  it 'navigates using breadcrumbs' do
+
+  end
 end
